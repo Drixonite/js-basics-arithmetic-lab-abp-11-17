a5767f380e4ad60c17a835d3e91e4e8c94233ed9
David Kim <mail.ddk@gmail.com>
2017-11-12 22:27:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ You should NOT try to redefine any of the above variables.
 // Write your code below this line!
 let newID = oldID + 1000000000;
 
-let ageIsValid = currentAge.isInteger();
+let ageIsValid = Number.isInteger();
 
 let randomNumber = Math.random() * 20;
 
